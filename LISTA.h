437e8714c5a76a89869561cb4266fcca4c043d73
sdellatorre <sdellatorre@gmail.com>
2018-09-06 17:26:32 -0300

    cabeceras 4 a 6

--- LISTA.h
+++ LISTA.h
@@ -17,7 +17,6 @@
 #define FALSE 0
 
 
-
 struct dato {
     int dato;
 } typedef Dato;
@@ -32,6 +31,27 @@ struct lista {
     int cantidadNodos;
     Nodo *primerNodo;
 } typedef Lista;
+
+// 04  
+// Precondición:La lista tiene que ser válida, el nodo tiene que ser válido y la posición debe ser un entero positivo
+// Entrada: Una lista, un nodo y una posición
+// Salida: Un boolean (True:El nodo fue insertado con exito en la posicion indicada / False:error en la insercion)
+// Postcondición: Se agrega un nuevo nodo en la posicion indicada de la lista.
+int inserteNodoPosicionLista (Lista *cabecera, Nodo nuevoNodo);
+
+//05
+// Precondición:La lista tiene que ser válida y el nodo tiene que ser válido 
+// Entrada: Una lista y un nodo
+// Salida: Un boolean (True:El nodo fue insertado con exito al final de la lista / False:error en la insercion)
+// Postcondición: Se agrega un nuevo nodo en el final  de la lista.
+void insertarNodoFinalLista (Lista *cabecera, Nodo nuevoNodo);
+
+// 06
+// Precondición:La lista tiene que ser válida 
+// Entrada: Una lista
+// Salida: Un nodo False:error en la insercion)
+// Postcondición: Se obtiene una copia del nodo que esta en la cabeza de la lista
+Nodo leerNodoInicioLista(Lista cabecera);
 
 #endif /* LISTA_H */
 
