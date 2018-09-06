2c1a99b4add140cea3b71f0965362bc99df39615
chaloS67 <gonzalo@gonzalo-X541UAK>
2018-09-06 16:32:49 -0300

    LISTA.H LISTA.C CREADOS

---
+++ LISTA.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   LISTA.h
+ * Author: gonzalo
+ *
+ * Created on 6 de septiembre de 2018, 16:31
+ */
+
+#ifndef LISTA_H
+#define LISTA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LISTA_H */
+
