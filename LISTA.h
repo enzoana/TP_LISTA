70ce8e4401baa5d60e8b9c3a9e8724dd8b96b5fe
sdellatorre <sdellatorre@gmail.com>
2018-09-06 16:59:41 -0300

    Constantes boolean

--- LISTA.h
+++ LISTA.h
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 /* 
  * File:   LISTA.h
@@ -8,6 +13,8 @@
 
 #ifndef LISTA_H
 #define LISTA_H
+#define TRUE 1
+#define FALSE 0
 
 struct dato {
     int dato;
